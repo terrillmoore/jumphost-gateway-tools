e73db3aada5f76d5d8ed5d664b61782fda6611e8
Terry Moore <tmm@mcci.com>
2018-03-17 19:25:29 +0000

    Add the scripts

---
+++ live-gateways.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TEMPDIR=/tmp/$$.d
+TEMPLOGGED=${TEMPDIR}/logged
+
+rm -rf $TEMPDIR
+mkdir -m 700 $TEMPDIR || { echo "Can't create temp dir" 1>&2 ; exit 1 ; }
+
+trap 'rm -rf $TEMPDIR' EXIT
+
+
+export LC_ALL=C
+netstat -ln | 
+	grep '::1:200' | 
+	awk '{ print $4 }' | 
+	sed -e 's/::1://' | sort > $TEMPLOGGED &&
+	sort -t: +2 -3 /etc/passwd | 
+	join -t: -1 1 -2 3 $TEMPLOGGED - | 
+	awk -F: '{ printf("%s\t%s\n", $2, $1) }' | 
+	sort
